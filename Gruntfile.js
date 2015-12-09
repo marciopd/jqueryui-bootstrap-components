e0860d4fca33b28784cc3d876646d6eca844eb6f
marciopd <marciopd@gmail.com>
2015-12-09 09:58:11 -0200

    Config grunt to run the unit tests.

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function(grunt) {
     },
     uglify: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+        banner: '/*! <%= pkg.name %> <%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
         mangle: true
       },
       my_target: {
@@ -17,14 +17,20 @@ module.exports = function(grunt) {
           'dist/jqbscomp.min.js':['src/js/button.js', 'src/js/dropdownMenuList.js', 'src/js/buttonDropdown.js', 'src/js/navs.js', 'src/js/breadcrumb.js', 'src/js/badge.js', 'src/js/pageHeader.js', 'src/js/alert.js', 'src/js/listGroup.js', 'src/js/listGroupLinkedItens.js', 'src/js/listGroupButtonItens.js']
         }
       }
+    },
+    karma: { 
+        unit: { 
+            configFile:'karma.conf.js'
+        }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-karma');
 
   // Default task(s).
-  grunt.registerTask('default', ['clean', 'uglify']);
+  grunt.registerTask('default', ['clean', 'uglify', 'karma']);
 
 };
